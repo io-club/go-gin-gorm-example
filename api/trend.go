2f03e1a5743a4708eb4c2b549ec29e7fbd95cfc8
DANG-chy <114589405+DANG-chy@users.noreply.github.com>
2023-03-27 19:21:16 +0800

    fix: 修复返回值

--- api/trend.go
+++ api/trend.go
@@ -27,7 +27,7 @@ func DeleteTrendById(c *gin.Context) {
 	}
 	err = model.DeleteTrendById(trendId)
 	if err != nil {
-		c.JSON(404, "err")
+		c.JSON(404, gin.H{"err": "id 解析失败"})
 	}
 
 }
@@ -35,14 +35,14 @@ func DeleteTrendById(c *gin.Context) {
 func GetTrendById(c *gin.Context) {
 	id := c.Param("id")
 	trendId, err := strconv.ParseInt(id, 10, 64)
-	trend, err :=model.GetTrendById(trendId)
+	trend, err := model.GetTrendById(trendId)
 	if err != nil {
-		c.JSON(404,"未查询到此id")
+		c.JSON(404, gin.H{"err": "id 解析失败"})
 		return
 	}
 	model.GetTrendById(trendId)
 	if err != nil {
-		c.JSON(400,"err:转换失败")
+		c.JSON(404, gin.H{"err": "trend 不存在"})
 		return
 	}
 	var images []model.Image
@@ -69,18 +69,18 @@ type GetTrendResponse struct {
 func CreateTrend(c *gin.Context) {
 	var req CreateTrendRequest
 	if err := c.ShouldBind(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "参数解析失败"})
 		return
 	}
 	filename := util.CreateFileName(req.PreviewImage)
 	if err := c.SaveUploadedFile(req.PreviewImage, "images/"+filename); err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+		c.JSON(500, gin.H{"error": "图片保存失败"})
 		return
 	}
 
 	trend := model.Trend{Name: req.Name, Type: req.Type, Detail: req.Detail, ImageURL: filename}
 	if err := model.DB.Create(&trend).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "保存失败"})
 		return
 	}
 
@@ -88,13 +88,13 @@ func CreateTrend(c *gin.Context) {
 	for _, file := range req.Images {
 		filename := util.CreateFileName(file)
 		if err := c.SaveUploadedFile(file, "images/"+filename); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "图片保存失败"})
 			return
 		}
 		images = append(images, model.Image{TableName: model.Trend{}.TableName(), RecordID: trend.ID, FileName: filename})
 	}
 	if err := model.CreateImages(images); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "图片保存失败"})
 		return
 	}
 
