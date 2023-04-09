51e3739004a1e1300f5126267d47e9f529eebb7c
trdthg <trdthg47@gmail.com>
2023-04-09 20:33:52 +0800

    feat: update Makefile

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
 include .env
 
 test:
-	echo ${DB_NAME}
+	echo DB_NAME: ${DB_NAME}
+	echo SERVER_SSH: ${SERVER_SSH}
+dev:
+	go run .
 build:
 	go build -ldflags "-linkmode=external -extldflags '-static'" .
 run:
 	./fabric
-dev:
-	go run .
 upload:
-	scp ./fibric root@101.42.21.155:/root/app/
-build_and_upload : build upload
-send:
-	scp ./fibric root@101.42.21.155:/root/app/
+	scp ./fibric ${SERVER_SSH}:/root/app/
+
+remote-stop:
+	ssh ${SERVER_SSH} "pkill fibric"
+remote-deploy:
+	ssh ${SERVER_SSH} "cd app && nohup ./fibric > log/fibric.log 2>&1 &"
+remote-clean:
+	ssh ${SERVER_SSH} "cd app && rm -f fibric"
+build_and_upload : build upload remote-deploy
--- curl.sh
+++ curl.sh
@@ -1,27 +1,28 @@
 #! /usr/bin/env bash
 
-export "$(grep -v '^#' .env | xargs)"
-# echo "Hello, ${DB_NAME}"
+set -eu
 
-dev=1
-if [ $dev -eq 1 ]; then
-  base_url="http://localhost:8080/api"
-else
-  base_url="http://101.42.21.155:80/api"
+BASE_URL="http://localhost:8080/api"
+echo "🤣 MODE: $MODE"
+if [ "${MODE}" = "release" ]; then
+  BASE_URL="http://101.42.21.155:80/api"
 fi
+echo "🤣 base url: $BASE_URL"
+
+set -eux
 
 # 00       00
 # service, api
 
 case $1 in
 1000) # register
-  curl -X POST "$base_url"/auth/register -d '{
+  curl -X POST "$BASE_URL"/auth/register -d '{
     "username": "test1",
     "password": "123"
   }'
   ;;
 1001) # login
-  curl -X POST "$base_url"/auth/login -d '{
+  curl -X POST "$BASE_URL"/auth/login -d '{
     "username": "test1",
     "password": "123"
   }'
@@ -31,26 +32,26 @@ case $1 in
   #     - page: int required, 例如: 1, 2, 3, ...
   #     - size: int required, 例如: 10, 20, 30, ...
   #     - category: string optional, 例如: defult, new, hot, ...
-  curl -X GET "$base_url/fabric/list?page=1&size=10&category=default"
+  curl -X GET "$BASE_URL/fabric/list?page=1&size=10&category=default"
   ;;
 2001) # get fabric
-  curl -X GET $base_url/fabric/"$2"
+  curl -X GET $BASE_URL/fabric/"$2"
   ;;
 2002) # post fabric
-  curl -X POST $base_url/fabric \
+  curl -X POST $BASE_URL/fabric \
     -F "name=aaa" \
     -F "detail=bbb" \
     -F "category=default" \
     -F "image=@${2}"
   ;;
 2003) # update fabric
-  curl -X PUT $base_url/fabric/"$2" \
+  curl -X PUT $BASE_URL/fabric/"$2" \
     -F "name=aaa1"
     # -F "detail=bbb2" \
     # -F "image=@"
   ;;
 2004) # delete fabric
-  curl -X DELETE $base_url/fabric/"$2"
+  curl -X DELETE $BASE_URL/fabric/"$2"
   ;;
 3001) # upload images
   # 上传图片
@@ -60,7 +61,7 @@ case $1 in
   # images: file required, 例如: a.jpg, b.jpg, ...
   #     图片可以传多个，但是数据库最多只能传 5 张
 
-  curl -X POST $base_url/image/upload \
+  curl -X POST $BASE_URL/image/upload \
     -F "tableName=$2" \
     -F "recordId=$3" \
     -F "images=@$4"
@@ -68,31 +69,31 @@ case $1 in
     # -F "images=@/home/trdthg/resources/a.jpg" \
   ;;
 3002) # delete image
-  curl -X DELETE $base_url/image/"$2"
+  curl -X DELETE $BASE_URL/image/"$2"
   ;;
 4000) # get brand list
   # query:
   #     - page: int required, 例如: 1, 2, 3, ...
   #     - size: int required, 例如: 10, 20, 30, ...
-  curl -X GET "$base_url/brand/list?page=1&size=10"
+  curl -X GET "$BASE_URL/brand/list?page=1&size=10"
   ;;
 4001) # get brand
-  curl -X GET $base_url/brand/"$2"
+  curl -X GET $BASE_URL/brand/"$2"
   ;;
 4002) # create brand
-  curl -X POST $base_url/brand \
+  curl -X POST $BASE_URL/brand \
     -F "name=aaa" \
     -F "detail=bbb" \
     -F "image=@${2}"
-  ;; 
+  ;;
 4003) # update brand
-  curl -X PUT $base_url/brand/"$2" \
+  curl -X PUT $BASE_URL/brand/"$2" \
     -F "name=aaa" \
     -F "detail=bbb2" \
     -F "image=@"
   ;;
 4004) # delete brand
-  curl -X DELETE $base_url/brand/"$2"
+  curl -X DELETE $BASE_URL/brand/"$2"
   ;;
 *)
   echo "Usage: $0 {get|post|put|delete}"
